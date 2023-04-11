e81b1e04ae3a7873a446f99cc231f724fbc4c8ee
coco <coco_look@outlook.com>
2023-04-11 14:57:04 +0800

    ll

--- field.go
+++ field.go
@@ -3,6 +3,7 @@ package validate
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 type Field struct {
@@ -56,12 +57,34 @@ func (f *Field) parse() *Field {
 	}
 	if !f.State {
 		if DebugModel {
-			f.Msg = fmt.Sprintf("field:%s value:%v verify:%s", f.Name, f.Val, t.GetMsg())
+			f.Msg = fmt.Sprintf("field:%s value:%v verify:%s", SnakeString(f.Name), f.Val, t.GetMsg())
 		} else {
-			f.Msg = fmt.Sprintf("field:%s verify:%s", f.Name, t.GetMsg())
+			f.Msg = fmt.Sprintf("field:%s verify:%s", SnakeString(f.Name), t.GetMsg())
 		}
 
 	}
 
 	return f
+}
+
+// 转为蛇形文
+func SnakeString(s string) string {
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	num := len(s)
+	for i := 0; i < num; i++ {
+		d := s[i]
+		// or通过ASCII码进行大小写的转化
+		// 65-90（A-Z），97-122（a-z）
+		//判断如果字母为大写的A-Z就在前面拼接一个_
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, '_')
+		}
+		if d != '_' {
+			j = true
+		}
+		data = append(data, d)
+	}
+	//ToLower把大写字母统一转小写
+	return strings.ToLower(string(data[:]))
 }
